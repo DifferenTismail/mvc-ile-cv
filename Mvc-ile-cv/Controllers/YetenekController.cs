216d10707386d9fad53124bf10ddbe42a8ca5c4e
İsmail Baran <ismailbaran04@gmail.com>
2024-10-15 20:04:08 +0300

    yetenek listeleme yapıldı

---
+++ Mvc-ile-cv/Controllers/YetenekController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mvc_ile_cv.Models.Entity;
+using Mvc_ile_cv.Repositories;
+namespace Mvc_ile_cv.Controllers
+{
+    public class YetenekController : Controller
+    {
+        GenericRepository<TBL_Yeteneklerim> repo = new GenericRepository<TBL_Yeteneklerim>();
+        public ActionResult Index()
+        {
+            var yetenekler = repo.List();
+            return View(yetenekler);
+        }
+    }
+}
