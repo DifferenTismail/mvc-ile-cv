8db2f3f81246d0828d23fe1a5cb2286da2a6e2ee
İsmail Baran <ismailbaran04@gmail.com>
2024-10-17 01:05:32 +0300

    egitim ekleme ve güncelleme kodları yazıldı

--- Mvc-ile-cv/Controllers/EgitimController.cs
+++ Mvc-ile-cv/Controllers/EgitimController.cs
@@ -3,17 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
 using Mvc_ile_cv.Models.Entity;
 using Mvc_ile_cv.Repositories;
 namespace Mvc_ile_cv.Controllers
 {
     public class EgitimController : Controller
     {
-        GenericRepository<TBL_Egitimlerim> repo = new GenericRepository<TBL_Egitimlerim> ();
+        GenericRepository<TBL_Egitimlerim> repo = new GenericRepository<TBL_Egitimlerim>();
         public ActionResult Index()
         {
             var egitim = repo.List();
             return View(egitim);
+        }
+        [HttpGet]
+        public ActionResult EgitimEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult EgitimEkle(TBL_Egitimlerim p)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("EgitimEkle");
+            }
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult EgitimGuncelle(int id)
+        {
+            var egitim = repo.Find(x => x.ID == id);
+            return View(egitim);
+        }
+        [HttpPost]
+        public ActionResult EgitimGuncelle(TBL_Egitimlerim t)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("EgitimEkle");
+            }
+            var egitim = repo.Find(x => x.ID == t.ID);
+            egitim.Baslik = t.Baslik;
+            egitim.AltBaslik_1 = t.AltBaslik_1;
+            egitim.AltBaslik_2 = t.AltBaslik_2;
+            egitim.Tarih = t.Tarih;
+            egitim.GNO = t.GNO;
+            repo.TUpdate(egitim);
+            return RedirectToAction("Index");
+        }
+        public ActionResult EgitimSil(int id)
+        {
+            var egitim = repo.Find(x => x.ID == id);
+            repo.TDelete(egitim);
+            return RedirectToAction("Index");
         }
     }
 }
