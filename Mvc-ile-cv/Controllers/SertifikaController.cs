b2fbdbda1080b69d483ecac85e7437ccc8a4f72c
İsmail Baran <ismailbaran04@gmail.com>
2024-10-17 01:46:12 +0300

    sertifika index ş

---
+++ Mvc-ile-cv/Controllers/SertifikaController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mvc_ile_cv.Models.Entity;
+using Mvc_ile_cv.Repositories;
+namespace Mvc_ile_cv.Controllers
+{
+    public class SertifikaController : Controller
+    {
+        GenericRepository<TBL_Sertifikalarim> repo = new GenericRepository<TBL_Sertifikalarim> ();
+        public ActionResult Index()
+        {
+            var sertifika = repo.List();
+            return View(sertifika);
+        }
+    }
+}
