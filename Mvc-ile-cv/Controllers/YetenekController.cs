ff5b8ee6a473a7f5b2c6bf87a20d077ea09d5884
İsmail Baran <ismailbaran04@gmail.com>
2024-10-16 23:49:48 +0300

    yetenek controller oluşturıldu ve sayfa ekleme silme güncelleme listeleme kodları yazıldı

--- Mvc-ile-cv/Controllers/YetenekController.cs
+++ Mvc-ile-cv/Controllers/YetenekController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Permissions;
 using System.Web;
 using System.Web.Mvc;
 using Mvc_ile_cv.Models.Entity;
@@ -14,6 +15,38 @@ namespace Mvc_ile_cv.Controllers
         {
             var yetenekler = repo.List();
             return View(yetenekler);
+        }
+        [HttpGet]
+        public ActionResult YeniYetenek()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniYetenek(TBL_Yeteneklerim p)
+        {
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+        public ActionResult YetenekSil(int id)
+        {
+            var yetenek = repo.Find(x => x.ID == id);
+            repo.TDelete(yetenek);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult YetenekGuncelle(int id)
+        {
+            var yetenek = repo.Find(x => x.ID == id);
+            return View(yetenek);
+        }
+        [HttpPost]
+        public ActionResult YetenekGuncelle(TBL_Yeteneklerim t)
+        {
+            var y = repo.Find(x => x.ID == t.ID);
+            y.Yetenek = t.Yetenek;
+            y.Oran = t.Oran;
+            repo.TUpdate(y);
+            return RedirectToAction("Index");
         }
     }
 }
