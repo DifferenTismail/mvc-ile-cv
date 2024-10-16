ecfa695734e93ceefb6852cc7dc85680c862e3a0
İsmail Baran <ismailbaran04@gmail.com>
2024-10-16 23:50:05 +0300

    eğitim controller açıldı

---
+++ Mvc-ile-cv/Controllers/EgitimController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mvc_ile_cv.Models.Entity;
+using Mvc_ile_cv.Repositories;
+namespace Mvc_ile_cv.Controllers
+{
+    public class EgitimController : Controller
+    {
+        GenericRepository<TBL_Egitimlerim> repo = new GenericRepository<TBL_Egitimlerim> ();
+        public ActionResult Index()
+        {
+            var egitim = repo.List();
+            return View(egitim);
+        }
+    }
+}
