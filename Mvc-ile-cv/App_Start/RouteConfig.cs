ff1aca0bdfc3975512fa45e0791169f54a117317
İsmail Baran <ismailbaran04@gmail.com>
2024-10-18 22:26:31 +0300

    route ayarı yapıldı

--- Mvc-ile-cv/App_Start/RouteConfig.cs
+++ Mvc-ile-cv/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Mvc_ile_cv
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Default", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Default", action = "Index", id = UrlParameter.Optional } 
             );
         }
     }
